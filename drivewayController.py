8110ce58a0ccfdc2d640559880de8a9298793e78
NR5P <orangev8zcar@gmail.com>
2020-11-06 19:20:16 -0600

    bug fixes

--- drivewayController.py
+++ drivewayController.py
@@ -1,35 +1,34 @@
-from umqttsimple import MQTTClient
+import paho.mqtt.client as MQTTClient
 import RPi.GPIO as GPIO     
 import vlc, time
 
 class DrivewayController():
     def __init__(self, client_id, mqtt_server, pin_num):
         self.client_id = client_id
-        self.client_id = ubinascii.hexlify(machine.unique_id())
+        self.client_id = "raspberrypiclient"
         self.mqtt_server = mqtt_server
-        self.topic = b"outside/driveway_sensor"
+        self.topic = "outside/driveway_sensor"
         self.light_pin = pin_num
         GPIO.setup(self.light_pin,GPIO.OUT)
 
-    def sub_cb(self, topic, msg):
-        print((topic, msg))
-        if topic == b'outside/driveway_sensor' and msg == b'true':
+    def on_msg(self, client, userdata, msg):
+        print(msg.topic+":"+str(msg.payload))
+        if msg.topic == b'outside/driveway_sensor' and msg.payload == b'true':
+            print("alarm firing")
             self.play_alarm()
             self.turn_light_on()
             self.stop()
 
     def connect_and_subscribe(self):
-        client = MQTTClient(self.client_id, self.mqtt_server)
-        client.set_callback(self.sub_cb)
-        client.connect()
+        client = MQTTClient.Client()
+        client.on_message = self.on_msg
+        client.connect('127.0.0.1',1883,60)
         client.subscribe(self.topic)
         print('Connected to %s MQTT broker, subscribed to %s topic' % (self.mqtt_server, self.topic))
         return client
 
     def restart_and_reconnect(self):
-        print('Failed to connect to MQTT broker. Reconnecting...')
-        time.sleep(10)
-        machine.reset()
+        pass
 
     def play_alarm(self):
         p = vlc.MediaPlayer("./alarm.mp3")
--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import time
-from umqttsimple import MQTTClient
-import micropython
-import network
 import RPi.GPIO as GPIO       
 from drivewayController import DrivewayController
 
 
-with open("settings.json") as file:
-    data = ujson.load(file.read())
-
-ssid = data["ssid"]
-password = data["password"]
 mqtt_server = "127.0.0.1" #localhost in this case
-client_id = ubinascii.hexlify(machine.unique_id())
+client_id = "raspimqttclient"
 
 last_message = 0
 message_interval = 20
@@ -21,29 +13,14 @@ LIGHT_PIN = 17
 
 GPIO.setmode(GPIO.BCM)       
 
-station = network.WLAN(network.STA_IF)
-station.active(True)
-station.connect(ssid, password)
+drivewayController = DrivewayController(client_id, mqtt_server, LIGHT_PIN)
 
-drivewayController = DrivewayController(client_id, mqtt_server)
-
-if station.isconnected() == False:
-    station.connect(ssid, password)
-while station.isconnected() == False:
-    pass
-print("Connection Successful")
-print(station.ifconfig())
 
 try:
     client = drivewayController.connect_and_subscribe()
+    print("Connection Successful")
 except OSError as e:
-    drivewayController.restart_and_reconnect()
+    print("error connecting")
 
-while True:
-  try:
-    client.check_msg()
-    if (time.time() - last_message) > message_interval:
-      last_message = time.time()
-      counter += 1
-  except OSError as e:
-    driveway_controller.restart_and_reconnect()
+client.loop_forever()
+
